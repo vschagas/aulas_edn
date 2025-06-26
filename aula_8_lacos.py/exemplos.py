c6a9fe205fe2eef734559521338a3dbe37db60e1
valmir <valmirchagas**@gmail.com>
2025-06-26 20:18:20 -0300

    aula 8

---
+++ aula_8_lacos.py/exemplos.py
@@ -0,0 +1,47 @@
+numbers = [1, 15, 3, 10, 5, 6, 8, 9]
+
+# for number in numbers:
+#     print(number)
+
+############################################
+# soma = 0
+# for number in numbers:
+#     soma += number
+
+# print(soma)
+############################################
+# result = 0
+# for i in numbers:
+#     if i % 2 == 0:
+#         result += 1
+
+# print(f"Existem {result} números pares")
+
+############################################
+
+# media = 0
+# for i in numbers:
+#     media += i
+
+# media = media / len(numbers)
+
+# print(media)
+
+###########################################
+
+frutas = ["uva", "banana", "pera"]
+
+# frutas.append("laranja")
+
+# frutas.remove("banana")
+
+# numbers.reverse()
+
+# numbers.sort(reverse = True)
+
+
+# print(sum(numbers))
+
+# print(max(numbers))
+
+# print(min(numbers))
