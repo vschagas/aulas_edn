7b83e91807d9e70865a0aade2bdb40a5a93dc03d
valmir <valmirchagas**@gmail.com>
2025-06-30 22:06:10 -0300

    exemplos aula 10

---
+++ aula_10/exemplo.py
@@ -0,0 +1,58 @@
+# def main():
+#     print("Executando o main")
+
+# if __name__ == "__main__":
+#     main()
+
+############################################################
+
+# import datetime
+# from datetime import datetime
+# from datetime import datetime as data
+
+# agora = data.now()
+# print("Data e hora atuais:", agora)
+
+# print("Ano:", agora.year)
+# print("Mês:", agora.month)
+
+#############################################################
+
+# import time
+
+
+# print("esperando 2 segundos...")
+# time.sleep(5)
+# print("pronto")
+
+
+
+
+##############################################################
+
+
+# import math
+
+# numero = 7.1
+
+# arredondado_para_baixo = math.floor(numero)
+# arredondado_para_cima = math.ceil(numero)
+
+# print(arredondado_para_baixo)
+# print(arredondado_para_cima)
+
+
+##############################################################
+
+# import random
+
+# alunos = ["clara", "helio", "danilo", "fabio"]
+
+
+# print(random.choice(alunos))
+
+# print(random.randint(1, 10))
+
+##############################################################
+
+import requests
