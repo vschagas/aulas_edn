e21b4742cbc4545fe34fbdbffa0a61b9bd9144ff
valmir <valmirchagas**@gmail.com>
2025-07-01 21:17:46 -0300

    exercicio 3

---
+++ aula_10/atividade3.py
@@ -0,0 +1,27 @@
+import requests
+
+
+def consultar_cep(value):
+    URL = f"https://viacep.com.br/ws/{value}/json/"
+
+    print(value)
+
+    response = requests.get(URL)
+
+    dados = response.json()
+    end = f"""
+    logradouro: {dados["logradouro"]}
+    bairro: {dados["bairro"]}
+    cidade: {dados["localidade"]}
+    estado: {dados["estado"]}
+    """
+
+    return end
+
+
+cep = input("Digite seu cep: ").replace("-","")
+
+result = consultar_cep(cep)
+
+print(result)
+
